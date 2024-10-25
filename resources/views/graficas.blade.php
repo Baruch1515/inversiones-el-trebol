e6c24f8d5935b1d1636d74af8a3a829f6b188da1
Baruch <65357980+Baruch1515@users.noreply.github.com>
2024-10-25 11:41:06 -0500

    Update graficas.blade.php MEJORA DE LOGICA

--- resources/views/graficas.blade.php
+++ resources/views/graficas.blade.php
@@ -1,5 +1,23 @@
+<head>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/0c07597779.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inversiones El trebol</title>
+    <style>
+        .table-dark-custom {
+            background-color: black;
+            color: white;
+        }
 
+        .table-dark-custom th,
+        .table-dark-custom td {
+            color: white;
+        }
+    </style>
+</head>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-3">
@@ -112,3 +130,7 @@
         }
     });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
+

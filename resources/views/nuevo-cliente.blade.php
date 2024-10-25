8660c302062d36e620d9f9e6de66ba1c677fdda8
Baruch <65357980+Baruch1515@users.noreply.github.com>
2024-10-25 11:41:41 -0500

    Update nuevo-cliente.blade.php

--- resources/views/nuevo-cliente.blade.php
+++ resources/views/nuevo-cliente.blade.php
@@ -1,3 +1,22 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/0c07597779.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inversiones El trebol</title>
+    <style>
+        .table-dark-custom {
+            background-color: black;
+            color: white;
+        }
+
+        .table-dark-custom th,
+        .table-dark-custom td {
+            color: white;
+        }
+    </style>
+</head>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-3">
